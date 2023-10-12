38d30f0f72b281ef52b397ac0db85971169ab529
Théo Ramousse <theo.ramousse@etu.uca.fr>
2023-10-12 16:10:35 +0200

    Added pictures

--- Server WebSocket/Server WebSocket/Program.cs
+++ Server WebSocket/Server WebSocket/Program.cs
@@ -8,10 +8,11 @@ class Program
     private static int _port = 5007;
     private static int _xMax = 700;
     private static int _yMax = 1000;
-    private static string connectionString = "Server=localhost;Database=mysql;User=root;Password=;";
+    private static string connectionString = "Server=localhost;Database=mysql;User=root;Password=;Port=3306";
 
     public static async Task Main()
     {
+        Console.WriteLine("Server starting, please wait 1 ou 2 minutes");
         MyWebSocketServer ws = new MyWebSocketServer(_xMax, _yMax, new DatabaseService(connectionString, _xMax, _yMax));
 
         ws.Setup(_port);
